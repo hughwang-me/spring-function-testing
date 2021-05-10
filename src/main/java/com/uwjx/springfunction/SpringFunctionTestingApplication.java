acbd574dc792becad9597355c9cd6d67ed24879d
wanghuan <18501667737@163.com>
2021-05-10 18:54:43 +0800

    更新

---
+++ src/main/java/com/uwjx/springfunction/SpringFunctionTestingApplication.java
@@ -0,0 +1,13 @@
+package com.uwjx.springfunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFunctionTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFunctionTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uwjx/springfunction/SpringFunctionTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uwjx.springfunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFunctionTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
